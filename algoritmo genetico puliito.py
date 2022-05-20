cbe3059b5fedb691642dedc0ef45d1543184b6b3
AndreaGuerry <105923438+AndreaGuerry@users.noreply.github.com>
2022-05-20 14:01:22 +0200

    Add files via upload

---
+++ algoritmo genetico puliito.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy
+import random
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+model=tf.keras.models.load_model(
+    'saved_model/my_model', custom_objects=None, compile=True, options=None
+)
+
+succesful_fail=[]
+
+def mutate(imagine):
+    coordinatex = random.randrange(0, 27)
+    coordinatey = random.randrange(0, 27)
+    variation = random.randrange(-5, 5)
+    variation = variation / 100
+    imagine[coordinatex, coordinatey]=imagine[coordinatex, coordinatey]+variation
+    if imagine[coordinatex, coordinatey]<0:
+        imagine[coordinatex, coordinatey]=0
+    elif imagine[coordinatex, coordinatey]>1:
+        imagine[coordinatex, coordinatey]=1
+    return imagine
+
+def fitnessFonction(imagine):
+    print(imagine.shape)
+    imagine=imagine.reshape(784)
+    imagine=imagine.squeeze()
+    print(imagine.shape)
+    predictionson=model(imagine).numpy()
+    risultatopredetto=numpy.argmax(predictionson)
+    risultatosecondo=numpy.argmax(predictionson.pop(risultatopredetto))
+    ff=risultatopredetto-risultatosecondo
+    return ff
+
+def compare(father, son):
+    fffather=fitnessFonction(father)
+    ffson=fitnessFonction(son)
+    if ffson<=0:
+        return son
+    elif fffather<=0:
+        return father
+#partie experimentale
+
+    else:
+        if ffson<fffather:
+            return son
+        else:
+            return father
+def algoritmo():
+        n=0
+        ancestor=x_train[0]
+
+        father=ancestor
+        son=mutate(father)
+
+        while n!=1:
+            father=compare(father, son)
+            if fitnessFonction(father)<0 or fitnessFonction(son)<0:
+                if fitnessFonction(father)<0:
+                    succesful_fail=father
+                else:
+                    succesful_fail=son
+                n=1
+                return succesful_fail
+                break
+            else:
+                n=0
+
+#print(imagine)
+print(algoritmo())
