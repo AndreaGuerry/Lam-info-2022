6967bcd8381f5e5d98b5c33ef99d67c6ec428260
AndreaGuerry <andrea.p.guerry@gmail.com>
2022-05-20 15:02:12 +0200

    modifica molto utile

--- algoritmo genetico puliito.py
+++ algoritmo genetico puliito.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy
 import random
+import matplotlib.pyplot as plt
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
@@ -8,14 +9,13 @@ model=tf.keras.models.load_model(
     'saved_model/my_model', custom_objects=None, compile=True, options=None
 )
 
-succesful_fail=[]
-
 def mutate(imagine):
     coordinatex = random.randrange(0, 27)
     coordinatey = random.randrange(0, 27)
-    variation = random.randrange(-5, 5)
+    variation = random.randrange(-10, 10)
     variation = variation / 100
     imagine[coordinatex, coordinatey]=imagine[coordinatex, coordinatey]+variation
+    print(imagine[coordinatex, coordinatey])
     if imagine[coordinatex, coordinatey]<0:
         imagine[coordinatex, coordinatey]=0
     elif imagine[coordinatex, coordinatey]>1:
@@ -23,13 +23,9 @@ def mutate(imagine):
     return imagine
 
 def fitnessFonction(imagine):
-    print(imagine.shape)
-    imagine=imagine.reshape(784)
-    imagine=imagine.squeeze()
-    print(imagine.shape)
-    predictionson=model(imagine).numpy()
+    predictionson=model(imagine.reshape(1, 28, 28)).numpy()
     risultatopredetto=numpy.argmax(predictionson)
-    risultatosecondo=numpy.argmax(predictionson.pop(risultatopredetto))
+    risultatosecondo=numpy.argmax(numpy.delete(predictionson, risultatopredetto))
     ff=risultatopredetto-risultatosecondo
     return ff
 
@@ -53,8 +49,9 @@ def algoritmo():
 
         father=ancestor
         son=mutate(father)
-
         while n!=1:
+            print("qui")
+            son = mutate(father)
             father=compare(father, son)
             if fitnessFonction(father)<0 or fitnessFonction(son)<0:
                 if fitnessFonction(father)<0:
@@ -62,10 +59,17 @@ def algoritmo():
                 else:
                     succesful_fail=son
                 n=1
+                img = father.reshape((28, 28))
+                plt.title('Image error ')
+                plt.imshow(img, cmap='gray')
+                plt.savefig('fail.png')
                 return succesful_fail
                 break
             else:
                 n=0
-
+                pass
 #print(imagine)
 print(algoritmo())
+imagine=x_train[:1]
+print(imagine.reshape(1, 28, 28).shape)
+print(x_train[:1].shape)
