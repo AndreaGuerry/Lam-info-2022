e446e518b837c63f4777272eeacdab7d527deb0d
AndreaGuerry <andrea.p.guerry@gmail.com>
2022-06-03 14:20:28 +0200

    aggiunta di un programma per verificare i risultati

--- algortimo genetico.py
+++ algortimo genetico.py
@@ -1,9 +1,10 @@
 from funzioni import algoritmo
 import matplotlib.pyplot as plt
+import numpy
 fail=algoritmo.algoritmo(0)
 print(fail)
-img = fail.reshape((28, 28))
 img = fail.reshape((28, 28))
 plt.title('Label is ')
 plt.imshow(img, cmap='gray')
 plt.savefig('risultati/fail1.png')
+numpy.save('risultati/array.npy', fail)
---
+++ test.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+import numpy
+set=numpy.load('risultati/array.npy')
+
+n=1
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+model=tf.keras.models.load_model(
+    'saved_model/my_model', custom_objects=None, compile=True, options=None
+)
+labelgiusto=y_train[n]
+risultato=model(set.reshape(1, 28, 28)).numpy()
+labeltest=numpy.argmax(risultato)
+print(labeltest)
+print(labelgiusto)
+if labeltest!=labelgiusto:
+    print("task succesfully failed")
+else:
+    print("c'è stato un problema")
