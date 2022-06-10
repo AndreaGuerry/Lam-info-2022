e6bdccac95f6e1d6a82c18627a0716ca29cd05d6
AndreaGuerry <andrea.p.guerry@gmail.com>
2022-06-10 14:21:30 +0200

    aggiunta di un programma per verificare i risultati

--- algortimo genetico.py
+++ algortimo genetico.py
@@ -1,10 +1,15 @@
 from funzioni import algoritmo
 import matplotlib.pyplot as plt
 import numpy
-fail=algoritmo.algoritmo(0)
+import tensorflow as tf
+num=0
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+result=y_train[num]
+fail=algoritmo.algoritmo(num)
 print(fail)
 img = fail.reshape((28, 28))
-plt.title('Label is ')
+plt.title('Image is not seen as a {result} by the ia"format(label=label)
 plt.imshow(img, cmap='gray')
 plt.savefig('risultati/fail1.png')
 numpy.save('risultati/array.npy', fail)
